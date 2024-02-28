bf2f9c81b896fb1954a2b125c176645d7679ba13
Abi-097 <120549516+Abi-097@users.noreply.github.com>
2024-02-28 23:13:41 +0530

    sidebar added

--- app/Components/Sidebar/Sidebar.js
+++ app/Components/Sidebar/Sidebar.js
@@ -87,6 +87,55 @@ const SidebarStyled = styled.nav`
       opacity: 0.2;
     }
     h1 {
+      font-size: 1.2rem;
+      display: flex;
+      flex-direction: column;
+      line-height: 1.5rem;
+    }
+    .image,
+    h1 {
+      position: relative;
+      z-index: 1;
+    }
+    .image {
+      flex-shrink: 0;
+      display: inline-block;
+      overflow: hidden;
+      transition: all 0.5s ease;
+      border-radius: 100%;
+      width: 70px;
+      height: 70px;
+    }
+    h1 {
+      margin-left: 1rem;
+      font-size: clamp(1.2rem, 4vw, 1.4rem);
+    }
+    &:hover {
+      .profile-overlay {
+        opacity: 1;
+        border: 2px solid ${(props) => props.theme.borderColor2};
+      }
+    }
+  }
+  .nav-item {
+    position: relative;
+    padding: 0.6rem 1rem 1rem 2.1rem;
+    margin: 0.3rem 0;
+    display: grid;
+    grid-template-columns: 40px 1fr;
+    gap: 15px;
+    cursor: pointer;
+
+    &::after {
+      position: absolute;
+      content: "";
+      left: 0;
+      top: 0;
+      width: 0;
+      height: 100vh;
+      background-color: ${(props) => props.theme.activeNavLinkHover};
+      z-index: 1;
+      transition: all 0.3s ease-in-out;
     }
   }
 `;
